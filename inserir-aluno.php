dd837a07cf27728e3d6d89316c99bc0ad38262e9
Matheus Rodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-05-30 10:54:27 +0000

    refactor: Update database connection in PHP files

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -4,8 +4,8 @@ use Alura\Pdo\Domain\Model\Student;
 
 require_once 'vendor/autoload.php';
 
-$databasePath = __DIR__ . 'banco.sqlite';
-$pdo = new PDO('sqlite:' . $databasePath);
+
+$pdo = \Alura\Pdo\Infrastructure\Persistence\ConnectionCreator::createConnection();
 
 echo 'Conectei' . PHP_EOL;
 
